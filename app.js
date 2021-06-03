1c48daa837238fc1e81e0f790333e1aa3dadaf8f
hazelisles <zhccj19@gmail.com>
2021-06-03 16:43:32 +0800

    feat: add dotenv

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const PORT = process.env.PORT
 require('./config/mongoose')
 
-app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: '.hbs', helpers: {
-  getCn: function(category, categories) {
-    const cn = categories.find(c => c.category === category)
-    return cn.category_cn
-  },
-  getIcon: function(category, categories) {
-    const icon = categories.find(c => c.category === category)
-    return icon.categoryIcon
+app.engine('hbs', exphbs({
+  defaultLayout: 'main', extname: '.hbs', helpers: {
+    getCn: function (category, categories) {
+      const cn = categories.find(c => c.category === category)
+      return cn.category_cn
+    },
+    getIcon: function (category, categories) {
+      const icon = categories.find(c => c.category === category)
+      return icon.categoryIcon
+    }
   }
-}
 }))
 app.set('view engine', 'hbs')
 
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense'
-mongoose.connect(MONGODB_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch(error => handleError(error))
 
 const db = mongoose.connection
