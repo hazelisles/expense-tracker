4247cba33c2ff230f50c6c5ff5d798dabc96c211
hazelisles <zhccj19@gmail.com>
2021-06-05 02:08:24 +0800

    feat: modify seeder and seeds.json"

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,13 +1,33 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require("../../config/mongoose")
 const Record = require('../record')
+const User = require('../user')
 const { record } = require('./seeds.json')
+const { user } = require('./seeds.json')
 
-db.once('open', () => {
-  Record.create(record)
-    .then(() => {
-      console.log('Record created!')
-      return db.close()
-    })
-    .then(() => console.log('Database connection closed'))
-    .catch(error => console.log(error))
+db.once('open', async () => {
+  try {
+    const pass = await Promise.all(Array.from({ length: 2 }, (value, i) => {
+      return bcrypt.hash(user[i].password, 10)
+    }))
+    await Promise.all(Array.from({ length: 2 }, async (v, i) => {
+      try {
+        let ur = await User.create({ name: user[i].name, email: user[i].email, password: pass[i] })
+        let records
+        for (let x = 5 * [i]; x < 5 * [i + 1]; x++) {
+          records = Object.assign(record[x], { userId: ur._id })
+          await Record.create(records)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }))
+    console.log('Done!')
+    process.exit()
+  } catch (err) {
+    console.warn(err)
+  }
 })
