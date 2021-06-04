ca5d49e2b8bd3a0fafe6462f6e681c4b1ffe0024
hazelisles <zhccj19@gmail.com>
2021-06-04 16:36:18 +0800

    feat: add facebook login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const localStrategy = require('passport-local').Strategy
+const facebookStrategy = require('passport-facebook').Strategy
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
@@ -20,6 +21,27 @@ module.exports = app => {
           return done(null, user)
         })
     }).catch(err => done(err, false))
+  }))
+
+  passport.use(new facebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, async (accessToken, refreshToken, profile, done) => {
+    try {
+      const { name, email } = profile._json
+      let user = await User.findOne({ email })
+      if (user) return done(null, user)
+      const randomPassword = Math.random().toString(36).slice(-8)
+      const hash = await bcrypt.hash(randomPassword, 10)
+      user = await User.create({
+        name, email, password: hash
+      })
+      return done(null, user)
+    } catch (err) {
+      done(err, false)
+    }
   }))
 
   passport.serializeUser((user, done) => {
